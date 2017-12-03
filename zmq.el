fd256ad4cb394ef641766e06180c3ebaa34ff471
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2017-12-03 09:57:13 -0600

    Remove unused and duplicated code

--- zmq.el
+++ zmq.el
@@ -81,58 +81,10 @@ finally closed."
 
 ;;; Subprocceses
 
-(defmacro zmq-async-process-environment (variables &rest body)
-  (declare (indent 1))
-  (let ((variable-forms
-         (seq-map (lambda (opt) `(setq-default
-                             ,opt ,(if (listp (symbol-value opt))
-                                       `(quote ,(symbol-value opt))
-                                     (symbol-value opt))))
-                  (seq-filter #'boundp variables))))
-    `(progn
-       (push ,(file-name-directory (locate-library "ffi")) load-path)
-       (push ,(file-name-directory (locate-library "zmq")) load-path)
-       (require 'zmq)
-       ,@variable-forms
-       ,@body)))
-
-(defun zmq-start-subprocess ()
-
-  )
-
-(zmq-async-process-environment ()
-  (while t
-    ;; listen for messages from kernel
-    ))
-
-;; TODO: Reduce load time in subprocesses by splitting functions to separate
-;; files. For example `zmq-core' can have all of the context objects and socket
-;; API's
-(defmacro zmq-start-socket (sock sock-type endpoint &rest body)
-  (declare (indent 3))
-  (async-start
-   `(lambda ()
-      (push ,(file-name-directory (locate-library "ffi")) load-path)
-      (push ,(file-name-directory (locate-library "zmq")) load-path)
-      (require 'zmq)
       (with-zmq-context ctx
-        (let ((,sock (zmq-socket ctx ,sock-type)))
-          (zmq-bind ,sock ,endpoint)
-          (unwind-protect
-              (progn ,@body)
-            (zmq-unbind ,sock ,endpoint)))))))
 
-;; zmq-bind-and-listen
 
-(defmacro with-zmq-context (ctx &rest body)
-  (declare (indent 1))
-  `(let ((,ctx (zmq-ctx-new)))
-     (unwind-protect
-         (progn ,@body)
-       (zmq-ctx-close))))
 
-(defun zmq-start-eventloop ()
 
-  )
 
 (provide 'zmq)
