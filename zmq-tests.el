74578cebb60a864707ee37f67db893ad647fe5a8
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2018-09-27 17:29:32 -0500

    Add standard header to `zmq-tests.el`

--- zmq-tests.el
+++ zmq-tests.el
@@ -1,4 +1,31 @@
-;;; -*- lexical-binding: t -*-
+;;; zmq-tests.el --- Tests for emacs-zmq-*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Nathaniel Nicandro
+
+;; Author: Nathaniel Nicandro <nathanielnicandro@gmail.com>
+;; Created: 27 Sep 2018
+;; Version: 0.0.1
+;; Keywords:
+;; X-URL: https://github.com/nathan/zmq-tests
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
 
 (require 'cl-lib)
 (require 'ert)
@@ -346,3 +373,5 @@
       (kill-process process))))
 
 (provide 'zmq-tests)
+
+;;; zmq-tests.el ends here
