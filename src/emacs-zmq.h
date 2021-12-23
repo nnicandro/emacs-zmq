cf2350dd61cba6554fd532598402887685b0f2d2
Daniel Kraus <daniel@kraus.my>
2021-12-23 14:13:55 +0100

    Do not export unnecessary symbols in dynamic library

--- src/emacs-zmq.h
+++ src/emacs-zmq.h
@@ -8,13 +8,27 @@
 #include "context.h"
 #include "poll.h"
 
-extern int plugin_is_GPL_compatible;
+// https://gcc.gnu.org/wiki/Visibility
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define ZMQ_EXPORT __attribute__ ((dllexport))
+  #else
+    #define ZMQ_EXPORT __declspec(dllexport)
+  #endif
+#else
+  #if __GNUC__ >= 4
+    #define ZMQ_EXPORT __attribute__ ((visibility ("default")))
+  #else
+    #define ZMQ_EXPORT
+  #endif
+#endif
 
+ZMQ_EXPORT int plugin_is_GPL_compatible;
 // Defined in constants.c
 extern void
 ezmq_expose_constants();
 
-extern int
+ZMQ_EXPORT int
 emacs_module_init(struct emacs_runtime *ert);
 
 #endif /* __EMACS_ZMQ_H__ */
