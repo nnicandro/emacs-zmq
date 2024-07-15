37d42dc37506234aa4b35bec6c98500e6f648363
Nathaniel Nicandro <nathanielnicandro@gmail.com>
2024-07-15 16:22:44 -0500

    Bump version

--- zmq.el
+++ zmq.el
@@ -1,4 +1,4 @@
-;;; zmq.el --- ZMQ bindings in elisp -*- lexical-binding: t -*-
+;;; zmq.el --- ZMQ bindings in Emacs-Lisp -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2018-2024 Nathaniel Nicandro
 
@@ -6,7 +6,7 @@
 ;; Created: 05 Jan 2018
 ;; URL: https://github.com/nnicandro/emacs-zmq
 ;; Keywords: comm
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Package-Requires: ((cl-lib "0.5") (emacs "26"))
 
 ;; This program is free software; you can redistribute it and/or
@@ -37,7 +37,7 @@
   "ZMQ bindings for Emacs"
   :group 'communication)
 
-(defconst zmq-emacs-version "v1.0.0")
+(defconst zmq-emacs-version "v1.0.1")
 
 (defvar zmq-current-context nil
   "The context set by the function `zmq-current-context'.")
